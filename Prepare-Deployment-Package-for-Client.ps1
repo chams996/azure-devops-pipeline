d06cef09cd2387a5bfb8a47f3b2d8cd30ff8f470
chams996 <161302168+chams996@users.noreply.github.com>
2025-12-03 05:56:13 +0100

    Update Prepare-Deployment-Package-for-Client.ps1

--- Prepare-Deployment-Package-for-Client.ps1
+++ Prepare-Deployment-Package-for-Client.ps1
@@ -1,30 +1,11 @@
 ﻿param (
     [string]$Path
 )
-# Created by Denis Molodtsov (@Zerg00s) in 2018
 
 $ErrorActionPreference = "Stop"
 
 $host.UI.RawUI.WindowTitle = "Flow & Power Apps Migrator"
 
-Write-host
-Write-host
-Write-Host "       ______ _                          _____                            " -ForegroundColor Yellow
-Write-Host "      |  ____| |                 ___    |  __ \                           " -ForegroundColor Yellow
-Write-Host "      | |__  | | _____      __  ( _ )   | |__) |____      _____ _ __      " -ForegroundColor Yellow
-Write-Host "      |  __| | |/ _ \ \ /\ / /  / _ \/\ |  ___/ _ \ \ /\ / / _ \ `'__|    " -ForegroundColor Yellow
-Write-Host "      | |    | | (_) \ V  V /  | (_>  < | |  | (_) \ V  V /  __/ |        " -ForegroundColor Yellow
-Write-Host "      |_|    |_|\___/ \_/\_/    \___/\/_|_|   \___/ \_/\_/ \___|_|        " -ForegroundColor Yellow
-Write-Host "          /\                     |  \/  (_)               | |             " -ForegroundColor Cyan
-Write-Host "         /  \   _ __  _ __  ___  | \  / |_  __ _ _ __ __ _| |_ ___  _ __  " -ForegroundColor Cyan
-Write-Host "        / /\ \ | `'_ \| `'_ \/ __| | |\/| | |/ _`` | `'__/ _`` | __/ _ \| `'__|" -ForegroundColor Cyan
-Write-Host "       / ____ \| |_) | |_) \__ \ | |  | | | (_| | | | (_| | || (_) | |   " -ForegroundColor Cyan
-Write-Host "      /_/    \_\ .__/| .__/|___/ |_|  |_|_|\__, |_|  \__,_|\__\___/|_|   " -ForegroundColor Cyan
-Write-Host "               | |   | |                    __/ |                        " -ForegroundColor Cyan
-Write-Host "               |_|   |_|                   |___/                         " -ForegroundColor Cyan
-Write-host                                                                             
-Write-host "-----------------------------------------------------------------------------"
-Write-host
 
 Write-Host "This script will produce a sharable package that your Client or Partner can use for deployments" -ForegroundColor Yellow
 
